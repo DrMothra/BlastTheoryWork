7dcbd00cdd3a25f8a24dbe8fa089c4479cec5fdd
Tony Glover <tony.glover@nottingham.ac.uk>
2015-02-10 18:59:57 +0000

    Created graphs from server data
    Generated a number of graphs from server data showing user mean values
    versus general mean values.

--- js/visData.js
+++ js/visData.js
@@ -2,7 +2,13 @@
  * Created by DrTone on 04/12/2014.
  */
 //Karen app with Blast Theory
+var margin = {top: 20, right: 60, bottom: 60, left: 80},
+    outerWidth = 512,
+    innerWidth = outerWidth - margin.left - margin.right,
+    outerHeight = 512,
+    innerHeight = outerHeight - margin.top - margin.bottom;
 
+var maxMeanValue = 5;
 
 $(document).ready(function() {
     //Retrieve data
@@ -28,48 +34,78 @@ $(document).ready(function() {
 
     //Visualise the received data
     function visualiseData(data) {
-        var margin = {top: 20, right: 60, bottom: 60, left: 80},
-            width = 512 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
+        //Get scales and means from data
+        var i = 0;
+        var userInfo = [];
 
-        var maxMeanValue = 5;
+        if (data.scales) {
+            for (i = 0; i < data.scales.length; ++i) {
+                var userMeanInfo = {};
+                userMeanInfo.name = data.scales[i].name;
+                userMeanInfo.userMean = data.scales[i].mean;
+                userInfo.push(userMeanInfo);
+            }
+        } else {
+            console.log('No scale data!');
+            return;
+        }
+
+        var aggScales = data.aggregate.scales ? data.aggregate.scales : null;
+        if (aggScales) {
+            for (i = 0; i < aggScales.length; ++i) {
+                //Assumes arrays in same order - check for this
+                userInfo[i].generalMean = aggScales[i].mean;
+            }
+        } else {
+            console.log('No aggregate scale data!');
+            return;
+        }
+
+        //Draw graphs with this data
+
+
+        var xStart = 0, yStart = 0, numColumns = 2, numRows = userInfo.length/numColumns;
+
+        for(var row=0; row<numRows; ++row) {
+            for (var col = 0; col < numColumns; ++col) {
+                drawGraph(xStart, yStart, userInfo[col+(row*2)]);
+                xStart += outerWidth;
+            }
+            xStart = 0;
+            yStart += outerHeight;
+        }
+
+    }
+
+    function drawGraph(xPos, yPos, data) {
 
         var svg = d3.select('#graph')
             .append('svg')
-            .attr({width: width + margin.left + margin.right,
-                height: height + margin.top + margin.bottom})
+            .attr({width: outerWidth,
+                height: outerHeight})
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
         var barWidth = 30;
         var interGap = 5;
-
-        if(data.scales) {
-            var meanData = [data.scales[0].mean, 2.5];
-        }
+        var meanData = [data.userMean, data.generalMean];
 
         var x = d3.scale.linear()
             .range([0, 150]);
 
         var y = d3.scale.linear()
-            .range([height, 0])
+            .range([innerHeight, 0])
             .domain([0, maxMeanValue]);
 
-        /*
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom");
-        */
-
         svg.append('text')
-            .attr("x", width/6)
-            .text(data.scales[0].name);
+            .attr("x", innerWidth/6)
+            .text(data.name);
 
         svg.append("line")
             .attr({x1: 0,
-                y1: height,
-                x2: width/2,
-                y2: height,
+                y1: innerHeight,
+                x2: innerWidth/2,
+                y2: innerHeight,
                 stroke: 'black',
                 'stroke-width': 1});
 
@@ -88,15 +124,22 @@ $(document).ready(function() {
             .attr("transform", function(d, i) {
                 i+=1.25;
                 return "translate(" + i * barWidth + ", 0)";
+            })
+            .style("fill", function(d, i) {
+                if(i%2) {
+                    return "steelblue";
+                } else {
+                    return "red";
+                }
             });
 
         bar.append("rect")
-            .attr("class", "bar")
             .attr("y", function(d) { return y(d); })
-            .attr("height", function(d) { return height - y(d); })
+            .attr("height", function(d) { return innerHeight - y(d); })
             .attr("width", barWidth - interGap);
 
         bar.append("text")
+            .attr("class", "barText")
             .attr("x", barWidth/2)
             .attr("y", function(d) { return y(d)+15; })
             .text(function(d) { return d > 0 ? d : null; });
